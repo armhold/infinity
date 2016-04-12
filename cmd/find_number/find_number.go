a2f3fdc974cfd12ad44fd1d59cabc93accf7d583
George Armhold <armhold@gmail.com>
2016-04-12 10:37:01 -0400

    find_number

---
+++ cmd/find_number/find_number.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/armhold/infinity"
+)
+
+var (
+	width int
+	file string
+)
+
+
+func init() {
+	flag.IntVar(&width, "w", 0, "breaks the output into lines of the given width")
+	flag.Parse()
+
+	// remaining args after flags are parsed
+	args := flag.Args()
+
+	if len(args) != 1 {
+		fmt.Fprintf(os.Stderr, "Usage: %s [-w N] file\n", os.Args)
+		os.Exit(1)
+	}
+	file = args[0]
+}
+
+
+// find_number interprets the bytes of the given file as a single large integer, and prints that integer to stdout.
+// NB: it reads the entire file into a byte array in memory, so it might not be appropriate for large files.
+func main() {
+	b, err := ioutil.ReadFile(file)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error reading file: %s %s:\n", file, err)
+		os.Exit(1)
+	}
+
+	decimalString := infinity.BytesToIntString(b)
+
+	if width == 0 {
+		// use full width- don't split the string at all
+		fmt.Printf("%s\n", decimalString)
+	} else {
+		for i, r := range decimalString {
+			fmt.Printf("%c", r)
+			if (i+1) % width == 0 {
+				fmt.Println()
+			}
+		}
+
+		fmt.Println()
+	}
+}
