f71e63028d6bbe9262e31db6b362c8a39ee325f1
George Armhold <armhold@gmail.com>
2016-04-15 08:03:04 -0400

    Support writing bytes to file in decode_number.

--- cmd/decode_number/decode_number.go
+++ cmd/decode_number/decode_number.go
@@ -7,22 +7,33 @@ import (
 
 	"github.com/armhold/infinity"
 	"io/ioutil"
+	"log"
 )
 
 var (
 	number string
-	file   string
+	inFile string
+	outFile string
 )
 
 func init() {
 	var usage = func() {
-		fmt.Fprintf(os.Stderr, "Usage: %s [-n number] [file]\n", os.Args[0])
-		flag.PrintDefaults()
+		u := `Usage: decode_number [-n number] [-o outfile] [file]
+
+If a file is given, the number to decode is read as text from that file.
+Else if [-n number] is given, the specified number is used.
+
+If an output file is specified, the decoded bytes are written to that file.
+Else they are printed to stdout.
+
+`
+		fmt.Fprintf(os.Stderr, u)
 	}
 
 	flag.Usage = usage
 
-	flag.StringVar(&number, "n", "", "decode given number")
+	flag.StringVar(&number, "n", "", "specifies a number to decode")
+	flag.StringVar(&outFile, "o", "", "specifies an output file in which to write the decoded bytes")
 	flag.Parse()
 
 	// remaining args after flags are parsed
@@ -34,24 +45,31 @@ func init() {
 	}
 
 	if len(args) > 0 {
-		file = args[0]
+		inFile = args[0]
 	}
 }
 
 // decode_number decodes the given number into its bytes, converts those bytes into a string, and
 // prints the string to stdout.
 func main() {
-	if file != "" {
-		s, err := ioutil.ReadFile(file)
+	if inFile != "" {
+		s, err := ioutil.ReadFile(inFile)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "error reading file: %s: %s ", file, err)
+			fmt.Fprintf(os.Stderr, "error reading file: %s: %s ", inFile, err)
 			os.Exit(1)
 		}
 		number = string(s)
 	}
 
 	bytes := infinity.IntStringToBytes(number)
-	s := string(bytes)
 
-	fmt.Fprintf(os.Stdout, "%s\n", s)
+	if outFile != "" {
+		err := ioutil.WriteFile(outFile, bytes, 0644)
+		if err != nil {
+			log.Fatalf("error writing output file: %s", err)
+		}
+	} else {
+		s := string(bytes)
+		fmt.Fprintf(os.Stdout, "%s\n", s)
+	}
 }
